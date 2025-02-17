3bc2a6354630b7e34d0fbc4089ccecb3f7614af5
AgDivyansh <93419706+AgDivyansh@users.noreply.github.com>
2025-02-17 07:59:10 +0000

    variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId = "DivyanshAg2002" ;
+let accountPassword = "Divyansh@2002" ;
+let accountEmail = "Divyanshag2209@gmail.com" ;
+let accountCity = "Bengaluru" ;
+console.table([accountId, accountPassword, accountEmail, accountCity]) ;
