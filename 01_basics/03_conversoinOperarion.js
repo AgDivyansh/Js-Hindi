adbf8cafcded593f3c244a9cb124f4b9c83b7665
AgDivyansh <93419706+AgDivyansh@users.noreply.github.com>
2025-02-17 08:55:22 +0000

    conversion of data type in js

---
+++ 01_basics/03_conversoinOperarion.js
@@ -0,0 +1,5 @@
+let score = "100" ;
+console.log(`data type of score is ${typeof(score)}`);
+
+let health = Number(score) ;
+console.log(`data type of health after type conversion of score to health ${typeof (health)}`);
