b5843b50dc43dc60efdb93793e1e5400c494c24f
AgDivyansh <93419706+AgDivyansh@users.noreply.github.com>
2025-02-17 09:25:19 +0000

    comparision in js

--- 01_basics/03_conversoinOperarion.js
+++ 01_basics/03_conversoinOperarion.js
@@ -3,3 +3,11 @@ console.log(`data type of score is ${typeof(score)}`);
 
 let health = Number(score) ;
 console.log(`data type of health after type conversion of score to health ${typeof (health)}`);
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+
