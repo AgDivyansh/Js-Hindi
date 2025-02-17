c07fa7c39725f8bf19343b681d0fa92d03fc1458
AgDivyansh <93419706+AgDivyansh@users.noreply.github.com>
2025-02-17 08:13:50 +0000

    data types in js

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,11 @@
+// Number
+// BigInt
+// String
+// Boolean
+// null
+// undefined
+// Symbol => unique
+// Object
+console.log(`type of null is : ${typeof (null)}`) ;
+console.log(`type of undefined is : ${typeof (undefined)}`) ;
+
