3f52182781d40feff889156e1d5a773617811808
AgDivyansh <93419706+AgDivyansh@users.noreply.github.com>
2025-02-18 10:19:33 +0000

    data types summary

--- 01_basics/04_comparisoin.js
+++ 01_basics/04_comparisoin.js
@@ -3,6 +3,8 @@ console.log(null <= 0);
 console.log(null > 0);
 console.log(null >= 0);
 console.log(null == 0);
+console.log(`when null is compared with undefined : ${null == undefined}`);
+
 
 console.log('2' >= 1);
 
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,22 @@
+// 7 primitives
+// call by value 
+
+// string 
+// Number
+// Boolean
+// null
+// undefined
+// Symbol
+// BigInt
+
+
+// refernce type data type 
+// pass by reference 
+
+// objects 
+// Array
+// functions
+
+const id1 = Symbol('123') ;
+const id2 = Symbol("123") ;
+console.log(` return value of Symbol data type is alwys unique even afer the same input : ${id1 == id2}`);
