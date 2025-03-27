a2671480df8db023c8e17888201868223ca8ad53
AgDivyansh <93419706+AgDivyansh@users.noreply.github.com>
2025-03-27 16:44:54 +0530

    Digital Clock

---
+++ Jacascirpt-Bacis-Porjects/3 - Digital clock/script.js
@@ -0,0 +1,10 @@
+// console.log(`every thing is working fine`);
+const clock = document.querySelector('#clock') ;
+
+
+setInterval(()=> {
+    let date = new Date() ;
+    clock.innerHTML = date.toLocaleTimeString() ;
+    console.log(date.toLocaleTimeString());
+    
+}, 1000) ;
